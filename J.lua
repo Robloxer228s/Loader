12f1a21fb1f39eb38f3750bdb8712d2fc2812d27
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2023-05-29 18:07:29 +0300

    Create J.lua

---
+++ J.lua
@@ -0,0 +1,14 @@
+_G.infinjump = true
+ 
+local Player = game:GetService("Players").LocalPlayer
+local Mouse = Player:GetMouse()
+Mouse.KeyDown:connect(function(k)
+if _G.infinjump then
+if k:byte() == 32 then
+Humanoid = game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+Humanoid:ChangeState("Jumping")
+wait(0.1)
+Humanoid:ChangeState("Seated")
+end
+end
+end)
