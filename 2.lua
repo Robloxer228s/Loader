89b46320a6b55019ef483cb8f62048c6e31f01ef
Robloxer228s <131464986+Robloxer228s@users.noreply.github.com>
2024-01-07 15:48:00 +0300

    Update 2.lua

--- 2.lua
+++ 2.lua
@@ -4,4 +4,4 @@ _G.SpamTim = "ChatSpam"
 _G.SpinTim = "SpeedSpin"
 _G.HideTim = true
 _G.OnDiedScript = game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/Fly/main/TimGui.lua")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/Fly/main/TimGui.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Robloxer228s/Fly/main/TimGui.lua"))
